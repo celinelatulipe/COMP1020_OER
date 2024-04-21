5525ef84278820664c27d5e6031a534095fd66ef
DevonBlewett <121255340+DevonBlewett@users.noreply.github.com>
2024-04-21 17:23:37 +0000

    more testing

---
+++ source/assets/Chapter2/Person.java
@@ -0,0 +1,13 @@
+	public class Person {
+		public String firstName;
+		public String lastName;
+		public int age;
+
+		public Person(String f, String l, int a) {
+			firstName = f;
+			lastName = l;
+			age = a;
+		}
+
+		 public static void main(String args[]) {} 
+	}
